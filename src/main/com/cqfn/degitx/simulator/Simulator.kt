ba0bbaa3d4cb457e771085009669d691aa455279
IlyaM <ilya.moskvitin@huawei.com>
2020-10-14 16:13:47 +0300

    New: Project stub to test CI

---
+++ src/main/com/cqfn/degitx/simulator/Simulator.kt
@@ -0,0 +1,21 @@
+package com.cqfn.degitx.simulator
+
+const val VERSION = "0.0.1"
+
+fun main() {
+    val Sim = Simulator()
+}
+
+class Simulator {
+    fun version(): String {
+        return "DeGitX Simulator v" + VERSION
+    }
+    // Read settings
+    // Initiate logs
+    // Generate Graph
+    // Place repositories to Nodes
+    // Run Simulation
+    // Stop Simulation
+    // Collect logs
+    // Export logs in some readable view
+}
