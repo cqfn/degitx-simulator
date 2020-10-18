b286606a97084847ca8c746ca63891240b3b1682
Ilya <25896691+IlyaMoskva@users.noreply.github.com>
2020-10-18 13:45:41 +0300

    Create build.gradle.kts
    Added gradle script to build project

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.10"
+    application
+}
+
+val kotlinVersion = "1.4.10"
+
+group = "com.cqfn.degitx.degitx-simulator"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    testImplementation("org.testng:testng-6.14.3")
+}
+
+val compileKotlin: KotlinCompile by tasks
+val compileTestKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions.jvmTarget = "11"
+compileTestKotlin.kotlinOptions.jvmTarget = "11"
+
+tasks.test {
+    // enable TestNG support (default is JUnit)
+    useTestNG()
+}
+
+application {
+    mainClass.set("com.cqfn.degitx.simulator.Simulator")
+}
